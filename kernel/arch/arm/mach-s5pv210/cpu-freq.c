1944ad032409e4b91d6854623124bc234d096447
Brian Beloshapka <bbelos@hotmail.com>
2011-12-07 23:16:24 -0600

    Define safe initial min and max speeds on boot, thanks to nubecoder.

--- kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -762,7 +762,11 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
                 g_dvfslockval[i] = MAX_PERF_LEVEL;
 #endif
 
-	return cpufreq_frequency_table_cpuinfo(policy, freq_table);
+	cpufreq_frequency_table_cpuinfo(policy, freq_table);
+	/* set default min and max policies to safe speeds */
+	policy->max = 1000000;
+	policy->min = 100000;
+	return 0;
 }
 
 static int s5pv210_cpufreq_notifier_event(struct notifier_block *this,
